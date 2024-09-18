da5810883fbe18a14c6223c4b9925bbf554ad1f1
arke0909 <157115247+arke0909@users.noreply.github.com>
2024-09-18 18:52:54 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct Coordinate
+{
+	int X = 0,Y = 0;
+};
+
+bool Compare(const Coordinate& a, const Coordinate b)
+{
+	if (a.X != b.X) return a.X < b.X;
+	return a.Y < b.Y;
+}
+
+int main()
+{
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+
+	cin >> n;
+	Coordinate *cd = new Coordinate[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> cd[i].X >> cd[i].Y;
+	}
+
+	sort(cd, cd + n, Compare);
+
+	for (int i = 0; i < n; i++)
+		cout << cd[i].X << ' ' << cd[i].Y << '\n';
+
+	return 0;
+}
