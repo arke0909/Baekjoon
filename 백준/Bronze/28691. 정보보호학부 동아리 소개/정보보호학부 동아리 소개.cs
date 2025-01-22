e90651a9f150242fd953a6023cfe6ed526bbdf31
EJY <157115247+arke0909@users.noreply.github.com>
2025-01-22 15:33:05 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 48 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cs
@@ -0,0 +1,20 @@
+string str = Console.ReadLine();
+
+switch(str)
+{
+    case "M":
+        Console.WriteLine("MatKor");
+        break;
+    case "W":
+        Console.WriteLine("WiCys");
+        break;
+    case "C":
+        Console.WriteLine("CyKor");
+        break;
+    case "A":
+        Console.WriteLine("AlKor");
+        break;
+    case "$":
+        Console.WriteLine("$clear");
+        break;
+}
