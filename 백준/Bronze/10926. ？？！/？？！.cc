865771a320be7bbbab3d1a95f924b374a008ab10
EJY <157115247+arke0909@users.noreply.github.com>
2025-03-10 10:34:51 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string id;
+
+	cin >> id;
+
+	cout << id << "??!";
+}
