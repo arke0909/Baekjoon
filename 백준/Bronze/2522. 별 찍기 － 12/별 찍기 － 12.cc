8b44531338c3a22b42ce1b2578e1b680b7e3b7c0
EJY <157115247+arke0909@users.noreply.github.com>
2025-04-11 21:35:11 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void Star(int n, int target)
+{
+	for (int i = 0; i < target - n; ++i)
+		cout << ' ';
+
+	for (int i = 0; i < n; ++i)
+		cout << '*';
+
+	cout << '\n';
+	
+	if (n == target) return;
+	
+	Star(n + 1, target);
+
+	for (int i = 0; i < target - n; ++i)
+		cout << ' ';
+
+	for (int i = 0; i < n; ++i)
+		cout << '*';
+
+	cout << '\n';
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	Star(1, n);
+
+	return 0;
+}
