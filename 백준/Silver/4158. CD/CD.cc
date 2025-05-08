e1823719c20d20ca79173de26403e482bccdb62f
EJY <157115247+arke0909@users.noreply.github.com>
2025-05-08 16:50:51 +0900

    [Silver V] Title: CD, Time: 992 ms, Memory: 87556 KB -BaekjoonHub

---
+++ 백준/Silver/4158. CD/CD.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	while (true)
+	{
+		int n, m, cnt = 0;
+		cin >> n >> m;
+
+		if (n == 0 && m == 0)
+			break;
+
+		unordered_map<int, int> map;
+
+		for (int i = 0; i < n + m; ++i)
+		{
+			int v;
+			cin >> v;
+			map[v]++;
+		}
+
+		for (auto item : map)
+		{
+			if (item.second > 1)
+				cnt++;
+		}
+
+		cout << cnt << '\n';
+	}
+
+
+}
