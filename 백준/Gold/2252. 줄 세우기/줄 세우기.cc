2ac0d6ff30707addb98fb1147204376ac4e3ddc2
EJY <157115247+arke0909@users.noreply.github.com>
2025-07-28 21:39:20 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 4148 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int n, m, root[32000];
+vector<int> graph[32000], save;
+
+void TopologySort()
+{
+	queue<int> q;
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (root[i] == 0) q.push(i);
+	}
+
+	while (!q.empty())
+	{
+		int v = q.front();
+		q.pop();
+
+		save.push_back(v + 1);
+
+		for (int i = 0; i < graph[v].size(); ++i)
+		{
+			root[graph[v][i]]--;
+			if (root[graph[v][i]] == 0)
+				q.push(graph[v][i]);
+
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < m; ++i)
+	{
+		int start, end;
+		cin >> start >> end;
+		graph[start - 1].push_back(end - 1);
+		root[end - 1]++;
+	}
+
+	TopologySort();
+
+	for (auto item : save)
+		cout << item << ' ';
+}
+
+
